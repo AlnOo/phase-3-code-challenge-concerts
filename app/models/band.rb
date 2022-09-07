3e1796b3250cfe02334e7484e5c6057c6a02cd7e
Allan <github email address>
2022-09-07 12:56:40 +0300

    update band initialize method

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,6 +1,6 @@
 class Band
 
-    attr_reader :name, :hometown
+  attr_reader :name, :hometown
   attr_writer :name
 
   @@all = []
@@ -15,16 +15,19 @@ class Band
     @@all
   end
 
+  #takes a venue and date (as a string) as arguments, and creates a new concert for the band in that venue on that date
   def play_in_venue(venue, date)
     Concert.new(date, self, venue)
   end
 
+  #should return an array of all the concerts that the band has played in
   def concerts
     Concert.all.find do |concert|
       concert.band == self
     end
   end
 
+   #returns an array of strings representing all the introductions for this band
   def self.all_introductions
     self.all.each do |band|
       puts "Hello, we are #{band.name} and we're from #{band.hometown}"
