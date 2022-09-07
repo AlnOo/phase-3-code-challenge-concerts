51480733c2d88b5329f79ebec9a6c113385eee19
Allan <github email address>
2022-09-07 12:35:08 +0300

    venue methods update

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,15 +1,34 @@
 class Venue
-    attr_accessor :title
-    attr_reader :city
-    @@all = []
+ 
+    attr_reader :title, :city
+  attr_writer :title
 
-    def initialize(title, city)
-        @title = title
-        @city = city
-        @@all << self
-    end
+  @@all = []
 
-    def self.all
-        @@all
+  def initialize(title, city)
+    @title = title
+    @city = city
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def concerts
+    Concert.all.find do |concert|
+      concert.venue == self
     end
+  end
+
+  def bands
+    Concert.all.find do |concert|
+      concert.venue == self
+    end.band
+  end
+
+  def self.most_hometown_shows
+
+  end
+
 end
