534cd081d9963d41712f30feee9a25af796bd19a
Allan <github email address>
2022-09-07 12:41:49 +0300

    venue dates included

--- tools/console.rb
+++ tools/console.rb
@@ -7,21 +7,21 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
-b1 = Band.new("Ragers", "glen")
-b2 = Band.new("Beats", "richmond")
-b3 = Band.new("Drum", "glenmont")
+b1 = Band.new("genge", "nonini")
+b2 = Band.new("noma", "frasha")
+b3 = Band.new("sugar", "ruby")
 
-v1 = Venue.new("msg", "brooklyn")
-v2 = Venue.new("capital", "dc")
-v3 = Venue.new("falcons", "atlanta")
+v1 = Venue.new("kondele", "kisumu")
+v2 = Venue.new("cabanas", "nairobi")
+v3 = Venue.new("parkcity", "mombasa")
 
 
-c1 = Concert.new("Nov. 3", b1, v1)
-c2 = Concert.new("Aug. 10", b2, v2)
-c3 = Concert.new("Mar. 11", b3, v3)
-c4 = Concert.new("Jul. 7", b1, v2)
-c5 = Concert.new("Jul. 10", b2, v3)
-c6 = Concert.new("Jul. 7", b2, v1)
+c1 = Concert.new("Jan. 8", b1, v1)
+c2 = Concert.new("Feb. 6", b2, v2)
+c3 = Concert.new("Mar. 5", b3, v3)
+c4 = Concert.new("Apr. 7", b1, v2)
+c5 = Concert.new("May. 3", b2, v3)
+c6 = Concert.new("Jun. 1", b2, v1)
 
 
 binding.pry
