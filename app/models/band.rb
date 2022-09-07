519b37d755568c3ebf2eef9a224b18225f225aa0
Allan <github email address>
2022-09-07 12:07:40 +0300

    initial commit

---
+++ app/models/band.rb
@@ -0,0 +1,17 @@
+class Band
+    attr_accessor :name
+    attr_reader :hometown
+    @@all = []
+
+    def initialize(name, hometown)
+        @name = name
+        @hometown = hometown
+        @@all << self
+    end
+
+    
+    
+    def self.all
+        @@all
+    end
+end
