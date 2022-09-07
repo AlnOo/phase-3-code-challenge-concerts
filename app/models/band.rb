747ca5a2fd4a7893e3c70c55f9527c0de6a8dc4a
Allan <github email address>
2022-09-07 12:32:39 +0300

    band.rb update

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,17 +1,34 @@
 class Band
-    attr_accessor :name
-    attr_reader :hometown
-    @@all = []
 
-    def initialize(name, hometown)
-        @name = name
-        @hometown = hometown
-        @@all << self
-    end
+    attr_reader :name, :hometown
+  attr_writer :name
 
-    
-    
-    def self.all
-        @@all
+  @@all = []
+
+  def initialize(name, hometown)
+    @name = name
+    @hometown = hometown
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def play_in_venue(venue, date)
+    Concert.new(date, self, venue)
+  end
+
+  def concerts
+    Concert.all.find do |concert|
+      concert.band == self
     end
+  end
+
+  def self.all_introductions
+    self.all.each do |band|
+      puts "Hello, we are #{band.name} and we're from #{band.hometown}"
+    end
+  end
+
 end
